7af48bb43b94475ff54ae9a160768d73bf9250b0
Gabriel Gazetta <31138728+ggazetta@users.noreply.github.com>
2023-12-01 17:27:15 -0500

    Update Test_A.py

--- Test_A.py
+++ Test_A.py
@@ -1,17 +1,20 @@
 import pytest
 import json
 import numpy as np
-import grades  
+from grades import gradeInfo
 
 with open('keys.json', 'r') as json_file:
     expected_results = json.load(json_file)
-    print(expected_results['example_4']['a'])
+random_files = random.sample(list(expected_results.keys()), 1)
 
-@pytest.mark.parametrize("filename, numExams, hwWeight, expected_ansA", [
-    ('grades_example_4.csv', 2, 0.3, expected_results['example_4']['a']),
-    # Add more test cases if needed
-])
-def test_ansA(filename, numExams, hwWeight, expected_ansA):
-    result = grades.gradeInfo(filename, numExams, hwWeight)
-    assert result[0] == expected_ansA
+def load_csv_data(csv_filepath):
+    return pd.read_csv(csv_filepath)
+
+@pytest.mark.parametrize("random_file", random_files)
+def test_ansA(random_file):
+    expected_count_trips = expected_results[random_file]["a"]
+    csv_file = random_file.replace('.json', '.csv')
+    actual_data = pd.read_csv(csv_file)
+    actual_A = gradeInfo(actual_data, 2, 0.3)
+    assert actual_count_trips == actual_A
 
