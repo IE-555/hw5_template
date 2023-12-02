418bf55959d473fba07161c26b28fab6a0f37a46
Gabriel Gazetta <31138728+ggazetta@users.noreply.github.com>
2023-12-02 09:49:03 -0500

    Add files via upload

--- Test_C.py
+++ Test_C.py
@@ -3,7 +3,6 @@ import json
 import numpy as np
 import pandas as pd
 import random
-import math
 from grades import gradeInfo
 
 with open('keys.json', 'r') as json_file:
@@ -11,11 +10,11 @@ with open('keys.json', 'r') as json_file:
 random_files = random.sample(list(expected_results.keys()), 1)
 
 
+        
 @pytest.mark.parametrize("random_file", random_files)
 def test_ansA(random_file):
-    expected = expected_results[random_file]["a"]
+    expected = expected_results[random_file]["c"]
     csv_file = random_file.replace('.json', '.csv')
-    actual_A = gradeInfo(csv_file, 2, 0.3)[0]
-    assert math.isclose(expected, actual_A, rel_tol=1e-9, abs_tol=1e-9)
-
+    actual_C = gradeInfo(csv_file, 2, 0.3)[2]
+    assert np.array_equal(expected, actual_C), "Arrays are not equal"
 
--- Test_D.py
+++ Test_D.py
@@ -3,7 +3,6 @@ import json
 import numpy as np
 import pandas as pd
 import random
-import math
 from grades import gradeInfo
 
 with open('keys.json', 'r') as json_file:
@@ -13,9 +12,8 @@ random_files = random.sample(list(expected_results.keys()), 1)
 
 @pytest.mark.parametrize("random_file", random_files)
 def test_ansA(random_file):
-    expected = expected_results[random_file]["a"]
+    expected = expected_results[random_file]["d"]
     csv_file = random_file.replace('.json', '.csv')
-    actual_A = gradeInfo(csv_file, 2, 0.3)[0]
-    assert math.isclose(expected, actual_A, rel_tol=1e-9, abs_tol=1e-9)
-
+    actual_D = gradeInfo(csv_file, 2, 0.3)[3]
+    assert expected == actual_D
 
